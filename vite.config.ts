f1fcd40223df8d2ab0748652c7e346ef21b8407a
Akiva Kraus <125896864+akiva1132@users.noreply.github.com>
2023-12-20 13:50:27 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,7 @@
-import { defineConfig, loadEnv } from "vite";
-import react from "@vitejs/plugin-react";
-export default defineConfig(({ command, mode }) => {
-  const env = loadEnv(mode, process.cwd(), "");
-  return {
-    plugins: [react()],
-    base: env.VITE_BASE ? `/${env.VITE_BASE}/` : "/",
-    server: {
-      watch: {
-        usePolling: true,
-      },
-      host: true,
-      strictPort: true,
-      port: 3000,
-    },
-  };
-});
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+})
